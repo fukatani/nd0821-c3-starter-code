a2104ebe3c72ec110ebcce2b56b093c8777bd729
Unknown <nannyakannya@gmail.com>
2024-08-25 17:09:45 +0900

    preprocess

--- starter/preprocess/preprocess.py
+++ starter/preprocess/preprocess.py
@@ -1,5 +1,5 @@
 import pandas
 
-df = pandas.read_csv("../data/census.csv")
+df = pandas.read_csv("./data/census.csv")
 df.drop_duplicates()
-df.to_csv("../data/cleaned_census.csv")
+df.to_csv("./data/cleaned_census.csv")
