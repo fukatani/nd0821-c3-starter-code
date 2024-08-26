b4ba7c6d688a2519338d729fa606f14d5a10846d
Unknown <nannyakannya@gmail.com>
2024-08-26 09:08:21 +0900

    metrics

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -4,6 +4,7 @@ import pickle
 
 from sklearn.model_selection import train_test_split
 import pandas
+import yaml
 
 # Add the necessary imports for the starter code.
 
@@ -11,7 +12,6 @@ from ml.data import process_data
 from ml.model import train_model, compute_model_metrics
 
 # Add code to load in the data.
-
 data = pandas.read_csv("./data/cleaned_census.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
@@ -52,5 +52,26 @@ print(f"recall: {recall}")
 print(f"fbeta: {fbeta}")
 pickle.dump(trained_model, open("model/trained_model.pkl", "wb"))
 pickle.dump(encoder, open("model/encoder.pkl", "wb"))
+metrics = {'precision': float(precision), 'recall': float(recall), 'fbeta': float(fbeta)}
+with open('model/train_metrics.yaml','w') as f:
+    yaml.dump(metrics, f, encoding='utf-8')
 
 # add data slicing
+for race in test["race"].unique():
+    sliced_test = test[test["race"] == race]
+    X_test, y_test, _, _ = process_data(
+        sliced_test,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb,
+    )
+    y_pred = trained_model.predict(X_test)
+    precision, recall, fbeta = compute_model_metrics(
+        y_test, y_pred
+    )
+    print(f"race: {race}")
+    print(f"precision: {precision}")
+    print(f"recall: {recall}")
+    print(f"fbeta: {fbeta}")
